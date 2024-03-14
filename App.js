9fe2427c47dfc505f07b86a189c1e3c63253478e
Jordin Gardner <jordin@dittowords.com>
2024-03-14 08:39:52 -0700

    successfully working on iOS simulator

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { DittoProvider, DittoComponent } from "ditto-react";
+import source from "./ditto";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DittoProvider source={source}>
+        <Text>
+          <DittoComponent componentId="example-component" />
+        </Text>
+        <StatusBar style="auto" />
+      </DittoProvider>
     </View>
   );
 }
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ ditto/index.js
@@ -0,0 +1,7 @@
+const components__root__base = require('./components__root__base.json');
+
+module.exports = {
+  "ditto_component_library": {
+    "base": {...components__root__base}
+  }
+}
